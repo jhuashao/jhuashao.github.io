55de8a0dd134b308cb0aa8f7d68c7f54f5015a1b
jhuashao <joshua.h.shao@gmail.com>
2017-11-24 18:04:48 -0800

    pushing existing site to jhuashao

---
+++ scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+		$('.risd')
+	.mouseover(function() {
+		$('body').css('background', 'url(gifs/risd.gif) no-repeat center center fixed');
+		$('body').css('background-size', 'cover');
+	})
+	.mouseout(function() {
+  		$('body').css('background', '');
+  		$('body').css('background-size', '');
+	});
+
+		$('.gd')
+	.mouseover(function() {
+		$('body').css('background', 'url(gifs/gd.gif) repeat left center fixed');
+		$('body').css('background-size', '10%');
+	})
+	.mouseout(function() {
+  		$('body').css('background', '');
+  		$('body').css('background-size', '');
+	});
+
+		$('.ctc')
+	.mouseover(function() {
+		$('body').css('background', 'url(gifs/ctc.gif) no-repeat center center fixed');
+		$('body').css('background-size', 'cover');
+	})
+	.mouseout(function() {
+  		$('body').css('background', '');
+  		$('body').css('background-size', '');
+	});
+
+		$('.cycling')
+	.mouseover(function() {
+		$('body').css('background', 'url(gifs/cycling.gif) repeat left center fixed');
+		$('body').css('background-size', '50%');
+	})
+	.mouseout(function() {
+		$('body').css('background', '');
+  		$('body').css('background-image', '');
+	});
+
+		$('.makingthings')
+	.mouseover(function() {
+		$('body').css('background', 'url(gifs/knots.gif) repeat center center fixed');
+		$('body').css('background-size', 'cover');
+	})
+	.mouseout(function() {
+  		$('body').css('background', '');
+  		$('body').css('background-size', '');
+	});
+});
