55dffd747c2c3baa151ae141e7dcf30f8265bd1c
jhuashao <joshua.h.shao@gmail.com>
2017-11-25 20:15:48 -0800

    cosmetic touches

--- scripts.js
+++ scripts.js
@@ -2,51 +2,51 @@ $(document).ready(function() {
 
 		$('.risd')
 	.mouseover(function() {
-		$('body').css('background', 'url(gifs/risd.gif) no-repeat center center fixed');
-		$('body').css('background-size', 'cover');
+		$('.maintextcontainer').css('background', 'url(gifs/risd.gif) repeat left center fixed');
+		$('.maintextcontainer').css('background-size', '50%');
 	})
 	.mouseout(function() {
-  		$('body').css('background', '');
-  		$('body').css('background-size', '');
+  		$('.maintextcontainer').css('background', '');
+  		$('.maintextcontainer').css('background-size', '');
 	});
 
 		$('.gd')
 	.mouseover(function() {
-		$('body').css('background', 'url(gifs/gd.gif) repeat left center fixed');
-		$('body').css('background-size', '10%');
+		$('.maintextcontainer').css('background', 'url(gifs/gd.gif) repeat left center fixed');
+		$('.maintextcontainer').css('background-size', '10%');
 	})
 	.mouseout(function() {
-  		$('body').css('background', '');
-  		$('body').css('background-size', '');
+  		$('.maintextcontainer').css('background', '');
+  		$('.maintextcontainer').css('background-size', '');
 	});
 
 		$('.ctc')
 	.mouseover(function() {
-		$('body').css('background', 'url(gifs/ctc.gif) no-repeat center center fixed');
-		$('body').css('background-size', 'cover');
+		$('.maintextcontainer').css('background', 'url(gifs/ctc.gif) no-repeat center center fixed');
+		$('.maintextcontainer').css('background-size', 'cover');
 	})
 	.mouseout(function() {
-  		$('body').css('background', '');
-  		$('body').css('background-size', '');
+  		$('.maintextcontainer').css('background', '');
+  		$('.maintextcontainer').css('background-size', '');
 	});
 
 		$('.cycling')
 	.mouseover(function() {
-		$('body').css('background', 'url(gifs/cyclingcompress.gif) repeat left center fixed');
-		$('body').css('background-size', '50%');
+		$('.maintextcontainer').css('background', 'url(gifs/cyclingcompress.gif) repeat left center fixed');
+		$('.maintextcontainer').css('background-size', '50%');
 	})
 	.mouseout(function() {
-		$('body').css('background', '');
-  		$('body').css('background-image', '');
+		$('.maintextcontainer').css('background', '');
+  		$('.maintextcontainer').css('background-image', '');
 	});
 
 		$('.makingthings')
 	.mouseover(function() {
-		$('body').css('background', 'url(gifs/knotscompress.gif) repeat center center fixed');
-		$('body').css('background-size', 'cover');
+		$('.maintextcontainer').css('background', 'url(gifs/knotscompress.gif) repeat center center fixed');
+		$('.maintextcontainer').css('background-size', 'cover');
 	})
 	.mouseout(function() {
-  		$('body').css('background', '');
-  		$('body').css('background-size', '');
+  		$('.maintextcontainer').css('background', '');
+  		$('.maintextcontainer').css('background-size', '');
 	});
 });
