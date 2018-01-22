f615da10e1d8d87761a9f9e92c4df631136031f7
jhuashao <joshua.h.shao@gmail.com>
2018-01-21 19:51:23 -0500

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,59 +1,68 @@
-$(document).ready(function() {
+// @codekit-prepend 'jquery-3.2.1.js'
+// @codekit-prepend 'jquery.color.js'
+// @codekit-prepend 'jquery.easing.1.3.js'
+// @codekit-prepend 'jquery.smoothState.js'
 
-		$('.risd')
-	.mouseover(function() {
-		$('.maintextcontainer').css('background', 'url(gifs/risd.gif) repeat left center fixed');
-		$('.maintextcontainer').css('background-size', '40%');
-	})
-	.mouseout(function() {
-  		$('.maintextcontainer').css('background', '');
-  		$('.maintextcontainer').css('background-size', '');
-	});
-
-		$('.gd')
-	.mouseover(function() {
-		$('.maintextcontainer').css('background', 'url(gifs/gd.gif) repeat left center fixed');
-		$('.maintextcontainer').css('background-size', '10%');
-	})
-	.mouseout(function() {
-  		$('.maintextcontainer').css('background', '');
-  		$('.maintextcontainer').css('background-size', '');
-	});
-
-		$('.ctc')
-	.mouseover(function() {
-		$('.maintextcontainer').css('background', 'url(gifs/ctc.gif) no-repeat center center fixed');
-		$('.maintextcontainer').css('background-size', 'cover');
-	})
-	.mouseout(function() {
-  		$('.maintextcontainer').css('background', '');
-  		$('.maintextcontainer').css('background-size', '');
-	});
-
-		$('.cycling')
-	.mouseover(function() {
-		$('.maintextcontainer').css('background', 'url(gifs/cyclingtrim.gif) no-repeat center center fixed');
-		$('.maintextcontainer').css('background-size', 'cover');
-	})
-	.mouseout(function() {
-		$('.maintextcontainer').css('background', '');
-  		$('.maintextcontainer').css('background-image', '');
-	});
-
-		$('.makingthings')
-	.mouseover(function() {
-		$('.maintextcontainer').css('background', 'url(gifs/knotscompress.gif) repeat center center fixed');
-		$('.maintextcontainer').css('background-size', 'cover');
-	})
-	.mouseout(function() {
-  		$('.maintextcontainer').css('background', '');
-  		$('.maintextcontainer').css('background-size', '');
-	});
-
-
+$(document).ready(function () {
+  $('ul.tabs').tabs();
+  swipeable: true;
+  responsiveThreshold: Infinity;
 });
 
-$('#seemore').click(function() {
-	$('html, body').animate({ scrollTop: 820 }, 600);
-	return false;
+$('.risd')
+  .mouseover(function () {
+    $('.maintextcontainer').css('background', 'url(gifs/risd.gif) repeat left center fixed');
+    $('.maintextcontainer').css('background-size', '40%');
+  })
+  .mouseout(function () {
+    $('.maintextcontainer').css('background', '');
+    $('.maintextcontainer').css('background-size', '');
+  });
+
+$('.gd')
+  .mouseover(function () {
+    $('.maintextcontainer').css('background', 'url(gifs/gd.gif) repeat left center fixed');
+    $('.maintextcontainer').css('background-size', '10%');
+  })
+  .mouseout(function () {
+    $('.maintextcontainer').css('background', '');
+    $('.maintextcontainer').css('background-size', '');
+  });
+
+$('.ctc')
+  .mouseover(function () {
+    $('.maintextcontainer').css('background', 'url(gifs/ctc.gif) no-repeat center center fixed');
+    $('.maintextcontainer').css('background-size', 'cover');
+  })
+  .mouseout(function () {
+    $('.maintextcontainer').css('background', '');
+    $('.maintextcontainer').css('background-size', '');
+  });
+
+$('.cycling')
+  .mouseover(function () {
+    $('.maintextcontainer').css('background', 'url(gifs/cyclingtrim.gif) no-repeat center center fixed');
+    $('.maintextcontainer').css('background-size', 'cover');
+  })
+  .mouseout(function () {
+    $('.maintextcontainer').css('background', '');
+    $('.maintextcontainer').css('background-image', '');
+  });
+
+$('.makingthings')
+  .mouseover(function () {
+    $('.maintextcontainer').css('background', 'url(gifs/knotscompress.gif) repeat center center fixed');
+    $('.maintextcontainer').css('background-size', 'cover');
+  })
+  .mouseout(function () {
+    $('.maintextcontainer').css('background', '');
+    $('.maintextcontainer').css('background-size', '');
+  });
+
+
+$('#seemore').click(function () {
+  $('html, body').animate({
+    scrollTop: 700
+  }, 600);
+  return false;
 });
